4fa25d421c73cb8d28870a54a8119e92dc20adbc
Yuan Gao <gaoyuankidult@gmail.com>
2013-10-30 05:34:02 +0200

    modification and deletion

--- yuan_gao_ex05/First_Exercise/ex01.f90
+++ yuan_gao_ex05/First_Exercise/ex01.f90
@@ -1,16 +1,41 @@
-program ackermann
-   integer :: ack
-   write(*,*) ack(3, 12) 
-end program ackermann
+module ackermann
+implicit none
+integer:: indicator = 0
+integer:: max = -1
+integer:: monitor
+contains
+  recursive function ack(m, n) result(a)
+    integer, intent(in) :: m,n
+    integer :: a
+    indicator = indicator +1
+    if (max <= indicator) then
+       max = indicator
+    end if
+    if(monitor == 1) then
+       print*,"endtered to next depth :",indicator
+    end if
 
-recursive function ack(m, n) result(a)
-  integer, intent(in) :: m,n
-  integer :: a
-  if (m == 0) then
-     a=n+1
-  else if (n == 0) then
-     a=ack(m-1,1)
-  else
-     a=ack(m-1, ack(m, n-1))
-  end if
-end function ack
+    if (m == 0) then
+       a=n+1
+    else if (n == 0) then
+       a=ack(m-1,1)
+    else
+       a=ack(m-1, ack(m, n-1))
+    end if
+    indicator = indicator -1
+    if(monitor == 1) then
+       print*,"return to previous depth",indicator
+    end if
+  end function ack
+end module ackermann
+
+program ackermann_prog
+  use ackermann
+  implicit none
+  integer::ack_value
+  print*,"Do you want to monitor the process ? (1 for yes, else for no)" 
+  read(*,*),monitor
+  ack_value = ack(2, 0) 
+  print*,"The answer of ackermann function is :", ack_value
+  print*,"The maximum depth of recursion is   :", max
+end program ackermann_prog
--- yuan_gao_ex05/Forth_Excercise/ex04.f90
+++ yuan_gao_ex05/Forth_Excercise/ex04.f90
@@ -1,81 +1,46 @@
-program matrix_multi_compare
-  real, pointer,dimension(:,:) :: randarray1
-  real, pointer,dimension(:,:) :: randarray2
-  real,pointer,dimension(:,:):: randarray3
-  real :: t1,t2,rate
-  integer :: c1,c2,cr,cm,i,j,n,s,p
-  integer , parameter :: x=20000,y=15000,runs=20
-  real :: array(x,y),a_diff,diff
-  open (unit = 2, file = "data.dat")
-  do p=20,800
-     print*,p,"of 20 to 800"
-     allocate(randarray1(p,p))
-     allocate(randarray2(p,p))
-     allocate(randarray3(p,p))
-     call random_number(randarray1)
-     call random_number(randarray2)
-     ! First initialize the system_clock
-     call system_clock(count_rate=cr)
-     call system_clock(count_max=cm)
-     rate = real(cr)
-     write(*,*) "system_clock rate ",rate
+program test_read
+  implicit none
+  character :: buffer
+  integer :: dist_array(10)
+  integer::sequence_number,longest_sequence
+  integer::ivalue,old_ivalue,long_seq_value
+  integer::io
+  dist_array = 0
+  sequence_number = 0
+  longest_sequence = 0
+  ivalue = 0
+  old_ivalue = 0
+  long_seq_value = 0
+  !open (unit=15, file="pi4e6.dat")  
+  open (unit=15, file="test.dat")
+  do while(1 == 1)
+     read (15,'(A1)',advance='NO', iostat = io),buffer
+     ivalue = ichar(buffer) -48
+     if(ivalue == old_ivalue) then
+        sequence_number=sequence_number +1
+        if(sequence_number > longest_sequence) then
+           longest_sequence=sequence_number
+           long_seq_value = ivalue
+        end if
+     else
+        sequence_number = 1
+     end if
+     old_ivalue = ivalue
 
-     print*,"System record for matmul matrix multiplication:"
-     diff = 0.0
-     a_diff = 0.0
-     s = 0
-     do n = 1 , runs
-        call CPU_TIME(t1)
-        call SYSTEM_CLOCK(c1)
-        randarray3=matmul(randarray2,randarray1)
-        call CPU_TIME(t2)
-        call SYSTEM_CLOCK(c2)
-        array(1,1) = array(1,2)     
-        if ( (c2 - c1)/rate < (t2-t1) ) s = s + 1
-        diff = (c2 - c1)/rate - (t2-t1) + diff
-        a_diff = ABS((c2 - c1)/rate - (t2-t1)) + a_diff
-     end do
-
-     write(*,*) "system_clock : ",(c2 - c1)/rate
-     write(*,*) "cpu_time     : ",(t2-t1)
-     write(*,*) "sc < ct      : ",s,"of",runs
-     write(*,*) "mean diff    : ",diff/runs
-     write(*,*) "abs mean diff: ",a_diff/runs
-     write(2,*),p
-     write (2,*),(t2-t1)
-     print *
-     print*,"System record for do loop matrix multiplication:"
-     diff = 0.0
-     a_diff = 0.0
-     s = 0
-     do n = 1 , runs
-        call CPU_TIME(t1)
-        call SYSTEM_CLOCK(c1)
-        do j=1, p
-           do i=1, p
-              do k=1, p
-                 randarray3(i,j)=randarray3(i,j)+randarray2(i,k)*randarray1(k,j)
-              enddo
-           enddo
-        enddo
-        call CPU_TIME(t2)
-        call SYSTEM_CLOCK(c2)
-        array(1,1) = array(1,2)     
-        if ( (c2 - c1)/rate < (t2-t1) ) s = s + 1
-        diff = (c2 - c1)/rate - (t2-t1) + diff
-        a_diff = ABS((c2 - c1)/rate - (t2-t1)) + a_diff
-     end do
-
-     write(*,*) "system_clock : ",(c2 - c1)/rate
-     write(*,*) "cpu_time     : ",(t2-t1)
-     write(*,*) "sc < ct      : ",s,"of",runs
-     write(*,*) "mean diff    : ",diff/runs
-     write(*,*) "abs mean diff: ",a_diff/runs
-     write (2,*),(t2-t1)
-     deallocate(randarray1)
-     deallocate(randarray2)
-     deallocate(randarray3)
+     if(ivalue >= 0 .and. ivalue < 10) then
+        dist_array(ivalue+1)=dist_array(ivalue+1) +1
+     end if
+     if (io .eq. -1) then
+        write(*,*)  "EOF encountered"
+        exit
+     end if
   end do
-  close(5)
-end program matrix_multi_compare
+  close(15)
 
+  print *, "The distribution trough 0 to 9 :"
+  print *, dist_array
+  print *, "Number with the longest sequence :"
+  print *, long_seq_value
+  print *, "The length of longest sequence :"
+  print *, longest_sequence
+end program test_read
